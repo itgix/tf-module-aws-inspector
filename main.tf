3360c6858708ce962ad3977ab13446fcddd9bcd6
Boris Yakimov <boris.yakimov@itgix.com>
2024-06-20 09:55:08 +0300

    #29 add aws inspector tf module

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_inspector2_enabler" "delegated_admin_account" {
+  count          = var.enable_amazon_inspector ? 1 : 0
+  account_ids    = [var.organization_management_account_id]
+  resource_types = var.amazon_inspector_resources_to_scan
+}
+
+resource "aws_inspector2_organization_configuration" "itgix_primary" {
+  count = var.enable_amazon_inspector ? 1 : 0
+  auto_enable {
+    ec2         = var.amazon_inspector_auto_enable_ec2_scanning
+    ecr         = var.amazon_inspector_auto_enable_ecr_scanning
+    lambda      = var.amazon_inspector_auto_enable_lambda_scanning
+    lambda_code = var.amazon_inspector_auto_enable_lambda_code_scanning
+  }
+
+  depends_on = [aws_inspector2_enabler.delegated_admin_account]
+}
+
+resource "aws_inspector2_member_association" "itgix_primary" {
+  count      = var.enable_amazon_inspector ? length(var.organization_member_account_ids) : 0
+  account_id = var.organization_member_account_ids[count.index]
+}
+
+resource "aws_inspector2_enabler" "member_accounts" {
+  count          = var.enable_amazon_inspector ? 1 : 0
+  account_ids    = var.organization_member_account_ids
+  resource_types = var.amazon_inspector_resources_to_scan
+}
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "enable_amazon_inspector" {
+  type        = bool
+  default     = false
+  description = "Enable or disable security hub"
+}
+
+variable "organization_management_account_id" {
+  type        = string
+  description = "The account ID of the organization managemnet account"
+  default     = ""
+}
+
+variable "organization_member_account_ids" {
+  type        = list(any)
+  description = "List of member account IDs where guarduty will be enabled"
+  default     = []
+}
+
+variable "amazon_inspector_resources_to_scan" {
+  type        = list(any)
+  default     = ["ECR", "EC2"]
+  description = "(Required) Type of resources to scan. Valid values are EC2, ECR, LAMBDA and LAMBDA_CODE. At least one item is required."
+}
+
+variable "amazon_inspector_auto_enable_ec2_scanning" {
+  type        = bool
+  default     = true
+  description = "(Required) Whether Amazon EC2 scans are automatically enabled for new members of your Amazon Inspector organization."
+}
+
+variable "amazon_inspector_auto_enable_ecr_scanning" {
+  type        = bool
+  default     = true
+  description = "(Required) Whether Amazon ECR scans are automatically enabled for new members of your Amazon Inspector organization."
+}
+
+variable "amazon_inspector_auto_enable_lambda_scanning" {
+  type        = bool
+  default     = false
+  description = "(Optional) Whether Lambda Function scans are automatically enabled for new members of your Amazon Inspector organization."
+}
+
+variable "amazon_inspector_auto_enable_lambda_code_scanning" {
+  type        = bool
+  default     = false
+  description = "(Optional) Whether AWS Lambda code scans are automatically enabled for new members of your Amazon Inspector organization. Note: Lambda code scanning requires Lambda standard scanning to be activated. Consequently, if you are setting this argument to true, you must also set the lambda argument to true"
+}
