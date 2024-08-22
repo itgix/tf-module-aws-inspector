622bdf83dde9558f0149cf617ae3c7f9c23c6951
Boris Yakimov <boris.yakimov@itgix.com>
2024-08-22 14:22:51 +0300

    add delegated admin config for sec acc

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
-resource "aws_inspector2_enabler" "delegated_admin_account" {
-  count          = var.enable_amazon_inspector ? 1 : 0
+resource "aws_inspector2_enabler" "itgix_primary" {
+  count          = var.inspector_organization_management_account ? 1 : 0
   account_ids    = [var.organization_management_account_id]
   resource_types = var.amazon_inspector_resources_to_scan
 }
 
+resource "aws_inspector2_delegated_admin_account" "delegated_inspector_admin_acc" {
+  count      = var.inspector_organization_management_account ? 1 : 0
+  account_id = var.organization_security_account_id
+}
+
 resource "aws_inspector2_organization_configuration" "itgix_primary" {
-  count = var.enable_amazon_inspector ? 1 : 0
+  count = var.inspector_organization_security_account ? 1 : 0
   auto_enable {
     ec2         = var.amazon_inspector_auto_enable_ec2_scanning
     ecr         = var.amazon_inspector_auto_enable_ecr_scanning
@@ -17,12 +22,12 @@ resource "aws_inspector2_organization_configuration" "itgix_primary" {
 }
 
 resource "aws_inspector2_member_association" "itgix_primary" {
-  count      = var.enable_amazon_inspector ? length(var.organization_member_account_ids) : 0
+  count      = var.inspector_organization_security_account ? length(var.organization_member_account_ids) : 0
   account_id = var.organization_member_account_ids[count.index]
 }
 
 resource "aws_inspector2_enabler" "member_accounts" {
-  count          = var.enable_amazon_inspector ? 1 : 0
+  count          = var.inspector_organization_security_account ? 1 : 0
   account_ids    = var.organization_member_account_ids
   resource_types = var.amazon_inspector_resources_to_scan
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,32 @@
-variable "enable_amazon_inspector" {
-  type        = bool
-  default     = false
-  description = "Enable or disable security hub"
-}
-
 variable "organization_management_account_id" {
   type        = string
   description = "The account ID of the organization managemnet account"
   default     = ""
 }
 
+variable "inspector_organization_management_account" {
+  type        = bool
+  default     = false
+  description = "Set to true when running from organization management account to configure the Guardduty delegated admin"
+}
+
+variable "inspector_organization_security_account" {
+  type        = bool
+  default     = false
+  description = "Set to true when running from organization security account to configure the Guardduty in the organization and invite member accounts"
+}
+
+
 variable "organization_member_account_ids" {
   type        = list(any)
   description = "List of member account IDs where guarduty will be enabled"
   default     = []
+}
+
+variable "organization_security_account_id" {
+  type        = string
+  description = "The account ID of the organization security account"
+  default     = ""
 }
 
 variable "amazon_inspector_resources_to_scan" {
